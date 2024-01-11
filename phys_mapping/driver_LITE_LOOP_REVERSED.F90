eda7f993482789427c0050f03fdf4e508eb7aaa2
Paul Mullowney <Paul.Mullowney@amd.com>
2024-01-11 11:22:52 -0500

    This branch includes restructured examples to better explore different offload strategies and compare to a pure hip version.

---
+++ phys_mapping/driver_LITE_LOOP_REVERSED.F90
@@ -0,0 +1,231 @@
+#define __NPROMA__ 64
+
+module mod
+  
+use :: iso_fortran_env
+
+implicit none
+
+integer, parameter :: sp = REAL32
+integer, parameter :: dp = REAL64
+#ifdef FLOAT_SINGLE
+integer, parameter :: lp = sp     !! lp : "local" precision
+#else
+integer, parameter :: lp = dp     !! lp : "local" precision
+#endif
+
+integer, parameter :: ip = INT32
+
+real(kind=lp) :: cst1 = 2.5, cst2 = 3.14
+integer, parameter :: nspecies = 5
+
+#ifdef OMP_DEVICE
+!$omp declare target(kernel)
+#endif
+
+contains
+
+subroutine kernel(dim1,dim2,i1,i2,in1,in2,in3,in4,in5,in6,in7,in8,in9,in10,out1)
+  integer(kind=ip),intent(in) :: dim1, dim2, i1,i2
+  real(kind=lp),dimension(1:dim1,1:dim2),intent(in) :: in2,in3,in4,in5,in6,in7,in8,in9,in10
+  real(kind=lp),dimension(1:dim1,1:dim2),intent(inout) :: in1, out1
+
+  integer(kind=ip) :: i,k
+#ifdef OPENACC
+!$acc routine vector
+!$acc loop vector private(i, k)
+#elif defined(OMP_DEVICE)
+!$omp parallel do simd
+#endif
+  do i=i1,i2
+    do k=1,dim2
+      out1(i,k) = (in1(i,k) + in2(i,k) + in3(i,k) + in4(i,k) + in5(i,k) + &
+ &                 in6(i,k) + in7(i,k) + in8(i,k) + in9(i,k) + in10(i,k)) * 0.1
+      in1(i,k) = out1(i,k)
+    end do
+  end do
+end subroutine kernel
+
+end module mod
+
+
+!
+! Main subroutine
+!
+
+program phys_layout
+
+use omp_lib
+
+use mod, only: ip, lp, kernel
+
+implicit none
+
+!! arrays for passing to subroutine
+real(kind=lp),dimension(:,:,:),allocatable :: arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10
+real(kind=lp),dimension(:,:,:),allocatable :: out1
+
+integer(kind=ip) :: nproma, npoints, nlev, nblocks, ntotal, num_main_loops, usesubroutine
+integer(kind=ip) :: nb, nml, inp, i, k, real_bytes
+real(kind=lp) :: est, dt, dttotal
+
+integer :: i_seed
+integer, dimension(:), allocatable :: a_seed
+
+integer :: iargs, lenarg
+character(len=20) :: clarg
+
+real(kind=lp) :: time1, time2
+
+! Constants
+nlev    = 137
+num_main_loops = 8
+
+! Defaults
+nproma = __NPROMA__
+ntotal = 64*16384
+usesubroutine = 1
+
+iargs = command_argument_count()
+if (iargs >= 1) then
+   call get_command_argument(1, clarg, lenarg)
+   read(clarg(1:lenarg),*) usesubroutine
+
+   if (iargs >= 2) then
+      call get_command_argument(2, clarg, lenarg)
+      read(clarg(1:lenarg),*) nproma
+
+      if (iargs >= 3) then
+         call get_command_argument(3, clarg, lenarg)
+         read(clarg(1:lenarg),*) ntotal
+      endif
+   endif
+endif
+npoints = nproma  ! Can be unlocked later...
+nblocks = ntotal / nproma
+write(*, '(A10,I8,A10,I8,A10,I8,A14,I8,A)') 'NPROMA ', nproma, ' TOTAL ', ntotal, ' NBLOCKS ', nblocks, ' USESUBROUTINE ', usesubroutine
+
+#ifdef FLOAT32
+real_bytes = 4
+#else
+real_bytes = 8
+#endif
+dttotal = 0
+allocate(arr1 (npoints,nlev,nblocks))
+allocate(arr2 (npoints,nlev,nblocks))
+allocate(arr3 (npoints,nlev,nblocks))
+allocate(arr4 (npoints,nlev,nblocks))
+allocate(arr5 (npoints,nlev,nblocks))
+allocate(arr6 (npoints,nlev,nblocks))
+allocate(arr7 (npoints,nlev,nblocks))
+allocate(arr8 (npoints,nlev,nblocks))
+allocate(arr9 (npoints,nlev,nblocks))
+allocate(arr10(npoints,nlev,nblocks))
+allocate(out1 (npoints,nlev,nblocks))
+
+! ----- Set up random seed portably -----
+call random_seed(size=i_seed)
+allocate(a_seed(1:i_seed))
+call random_seed(get=a_seed)
+a_seed = [ (i,i=1,i_seed) ]
+call random_seed(put=a_seed)
+deallocate(a_seed)
+
+! #ifdef OMP_HOST
+! Initialize values in parallel region to avoid accidental
+! NUMA issues in single-socket mode on dual-socket machines.
+!$omp parallel
+
+!$omp master
+! #endif
+do inp = 1, ntotal, nproma
+  nb = (inp-1) / nproma + 1
+  call random_number(arr1(:,:,nb))
+  call random_number(arr2(:,:,nb))
+  call random_number(arr3(:,:,nb))
+  call random_number(arr4(:,:,nb))
+  call random_number(arr5(:,:,nb))
+  call random_number(arr6(:,:,nb))
+  call random_number(arr7(:,:,nb))
+  call random_number(arr8(:,:,nb))
+  call random_number(arr9(:,:,nb))
+  call random_number(arr10(:,:,nb))
+end do
+! #ifdef OMP_HOST
+!$omp end master
+
+!$omp end parallel
+! #endif
+
+! Data offload
+#ifdef OMP_DEVICE
+print *, "Number of available devices", omp_get_num_devices()
+!$omp target data map(tofrom:arr1) map(to:arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10) map(from:out1)
+#elif defined(OPENACC)
+!$acc data copy(arr1) copyin(arr2, arr3, arr4, arr5, arr6, arr7, arr8, arr9, arr10) copyout(out1)
+#endif
+
+time1 = omp_get_wtime()
+
+do nml=1,num_main_loops
+
+! The classic BLOCKED-NPROMA structure from the IFS
+   if (usesubroutine == 0) then
+
+#ifdef OMP_HOST
+!$omp parallel do schedule(static) default(shared) private(nb,inp)
+#elif defined(OMP_DEVICE)
+!$omp target teams distribute parallel do collapse(2) thread_limit(__NPROMA__)
+#elif defined(OPENACC)
+!$acc parallel loop gang private(nb, inp, i) vector_length(__NPROMA__)
+#endif
+      do nb=1,nblocks
+         do i=1_ip, nproma
+            do k=1,nlev
+               out1(i,k,nb) = (arr1(i,k,nb) + arr2(i,k,nb) + arr3(i,k,nb) + arr4(i,k,nb) + arr5(i,k,nb) + &
+                    &                    arr6(i,k,nb) + arr7(i,k,nb) + arr8(i,k,nb) + arr9(i,k,nb) + arr10(i,k,nb)) * 0.1
+               arr1(i,k,nb) = out1(i,k,nb)
+            end do
+         end do
+      end do
+
+   else
+
+#ifdef OMP_HOST
+!$omp parallel do schedule(static) default(shared) private(nb,inp)
+#elif defined(OMP_DEVICE)
+!$omp target teams distribute thread_limit(__NPROMA__)
+#elif defined(OPENACC)
+!$acc parallel loop gang private(nb, inp, i) vector_length(__NPROMA__)
+#endif
+      do nb=1,nblocks
+         call kernel( nproma, nlev, 1_ip, nproma, arr1(:,:,nb), arr2(:,:,nb), &
+              &           arr3(:,:,nb), arr4(:,:,nb), arr5(:,:,nb), &
+              &           arr6(:,:,nb), arr7(:,:,nb), arr8(:,:,nb), &
+              &           arr9(:,:,nb), arr10(:,:,nb), out1(:,:,nb) )
+
+      end do
+
+   endif
+
+end do !nml
+
+time2 = omp_get_wtime()
+dttotal = time2-time1
+
+
+#ifdef OMP_DEVICE
+!$omp end target data
+#elif OPENACC
+!$acc end data
+#endif
+
+write(*, '(A,3F12.6)') 'Result check : ', arr1(1,1,1), sum(arr1) / real(npoints*nlev*nblocks), sum(out1) / real(npoints*nlev*nblocks)
+write(*, '(A,F8.4)') 'Time for kernel call : ', dttotal
+est=1.e-6*num_main_loops*11*npoints*nlev*nblocks*real_bytes
+write(*, '(A,F12.2,A,F12.2,A)') 'Bandwidth estimate : ', est, &
+     ' MB transferred: ', est / dttotal, ' MB/s '
+
+! TODO: Should really dealloc all this stuff... :(
+
+end program phys_layout
---
+++ phys_mapping/hip.cpp
@@ -0,0 +1,106 @@
+#include <chrono>
+#include "hip/hip_runtime.h"
+
+#define HIP_CALL(call)                                   \
+	do {                                                  \
+	hipError_t err = call;                                \
+	if (hipSuccess != err) {                              \
+	printf("HIP ERROR (code = %d, %s) at %s:%d\n", err,   \
+			 hipGetErrorString(err), __FILE__, __LINE__);   \
+	assert(0);                                            \
+	exit(1);                                              \
+	}                                                     \
+} while (0)
+
+__global__ void phys_kernel(const int N,
+									 double * __restrict__ in1,
+									 const double * __restrict__ in2,
+									 const double * __restrict__ in3,
+									 const double * __restrict__ in4,
+									 const double * __restrict__ in5,
+									 const double * __restrict__ in6,
+									 const double * __restrict__ in7,
+									 const double * __restrict__ in8,
+									 const double * __restrict__ in9,
+									 const double * __restrict__ in10,
+									 double * __restrict__ out1)
+{
+	int tid = threadIdx.x + blockIdx.x*blockDim.x;
+	if (tid<N)
+	{
+		out1[tid] = (in1[tid] + in2[tid] + in3[tid] + in4[tid] + in5[tid] +
+						 in6[tid] + in7[tid] + in8[tid] + in9[tid] + in10[tid])*0.1;
+		in1[tid] = out1[tid];
+	}
+}
+
+extern "C"
+{
+	void phys_kernel_lite_loop_hip ( int* dim1, int* dim2, int* dim3,
+												int* i1, int* i2, int * nt,
+												double * in1,
+												double * in2,
+												double * in3,
+												double * in4,
+												double * in5,
+												double * in6,
+												double * in7,
+												double * in8,
+												double * in9,
+												double * in10,
+												double * out1,
+												double * dt)
+	{
+		double *din1, *din2, *din3, *din4, *din5, *din6, *din7, *din8, *din9, *din10, *dout1;
+		int N = (*dim1)*(*dim2)*(*dim3);
+		//printf("dim1=%ld, dim2=%ld, dim3=%ld, in1(1,1,1)=%1.15g, in1(1,1,2)=%1.15g\n",*dim1,*dim2,*dim3,*in1,*(in1+1));
+		HIP_CALL(hipMalloc((void**)&din1, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din2, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din3, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din4, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din5, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din6, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din7, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din8, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din9, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&din10, N*sizeof(double)));
+		HIP_CALL(hipMalloc((void**)&dout1, N*sizeof(double)));
+		HIP_CALL(hipMemcpy(din1,in1,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din2,in2,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din3,in3,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din4,in4,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din5,in5,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din6,in6,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din7,in7,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din8,in8,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din9,in9,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(din10,in10,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(dout1,out1,N*sizeof(double),hipMemcpyHostToDevice));
+
+		const int nthreads = *nt;
+		const int nblocks = (N+nthreads-1)/nthreads;
+		printf("N=%d, nblocks=%d\n",N,nblocks);
+		auto start = std::chrono::high_resolution_clock::now();
+		phys_kernel<<<nblocks, nthreads>>>(N,din1,din2,din3,din4,din5,din6,din7,din8,din9,din10,dout1);
+		HIP_CALL(hipGetLastError());
+		HIP_CALL(hipDeviceSynchronize());
+
+		auto stop = std::chrono::high_resolution_clock::now();
+		*dt = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() * 1.e-9;
+
+		HIP_CALL(hipMemcpy(out1,dout1,N*sizeof(double),hipMemcpyHostToDevice));
+		HIP_CALL(hipMemcpy(in1,din1,N*sizeof(double),hipMemcpyHostToDevice));
+
+		HIP_CALL(hipFree(din1));
+		HIP_CALL(hipFree(din2));
+		HIP_CALL(hipFree(din3));
+		HIP_CALL(hipFree(din4));
+		HIP_CALL(hipFree(din5));
+		HIP_CALL(hipFree(din6));
+		HIP_CALL(hipFree(din7));
+		HIP_CALL(hipFree(din8));
+		HIP_CALL(hipFree(din9));
+		HIP_CALL(hipFree(din10));
+		HIP_CALL(hipFree(dout1));
+	}
+}
